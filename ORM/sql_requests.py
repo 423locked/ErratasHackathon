f06f1694acde4bc89f80e113cd961434d0eb6eeb
BilboBaGG <xxxeromant@gmail.com>
2022-10-23 02:20:12 +0300

    sql 1

--- ORM/sql_requests.py
+++ ORM/sql_requests.py
@@ -1,10 +1,38 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, JSON
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 from configs.hosts import *
 
-host_url = f"postgers://{HOST_USER}:{HOST_PASS}@{HOST_IP}:{HOST_PORT}/{DB_NAME}"
+declarativeDataBase = declarative_base()
 
-class SQLrequest:
-	@staticmethod
-	def register_user():
-		pass
+class NewUserData(declarativeDataBase):
+    __tablename__ = "UserData"
+
+    id = Column("id", String)
+
+    firstname = Column('firstname', String)
+    lastname = Column('lastname', String)
+    identifiers = Column({"mail":"value", "phone":"value"}, JSON)
+
+    def __repr__(self):
+        return "".format(self.code)
+
+
+class SQLrequests:
+
+	def __init__(self):
+		host_url = f"postgresql://{HOST_USER}:{HOST_PASS}@{HOST_IP}:{HOST_PORT}/{DB_NAME}"
+		self.dataBase = create_engine(db_string)
+
+
+	def register_user(username, password, name, surname, phone="None", mail="None"):
+		session = self.Session_class() # init class
+
+		user = NewUserData(id = "123456789012345678", firstname = name, lastname = surname, identifiers= {"mail": mail,"phone":phone})
+		print(user.id,user.identifiers)
+
+
+s = SQLrequests()
+
+s.register_user(username="alah",password="asd",name = "Bob", surname = "Dilan")
