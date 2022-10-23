786af2b1f7020df8c4431b35bfb21246bec174f4
423locked <yaroslav05042005@gmail.com>
2022-10-23 14:21:37 +0300

    Stable orm to remote db UserData connection

--- ORM/sql_requests.py
+++ ORM/sql_requests.py
@@ -7,29 +7,35 @@ from configs.geturl import getUrlByConfigs
 declarativeDataBase = declarative_base()
 
 
-'''class UserRegister(declarativeDataBase):
+class UserRegister(declarativeDataBase):
     __tablename__ = "UserData"
 
-    id = Column("id", String)
+    id = Column("id", String, primary_key=True)
 
     firstname = Column('firstname', String)
+    middlename = Column('middlename', String)
     lastname = Column('lastname', String)
     identifiers = Column(JSON)
 
     def __repr__(self):
-        return "".format(self.code)'''
+        return "".format(self.code)
 
 
 def get_engine():
     return create_engine(getUrlByConfigs())
 
 
+#https://www.compose.com/articles/using-postgresql-through-sqlalchemy/
 class ORM:
     @staticmethod
     def register_user(username, password, name, surname, phone="None", mail="None"):
-        session_class = sessionmaker(get_engine())
-        session = session_class()# init class
+        Session = sessionmaker(get_engine())
+        session = Session()# init class
 
-        #user = UserRegister(id = "123456789012345678", firstname = name, lastname = surname, identifiers= {"mail": mail,"phone":phone})
-        #print(user.id, user.identifiers)
-        print('aint no way')
+        user = UserRegister(id="123456789012345678", firstname = name, middlename='Bebra', lastname = surname, identifiers= {"mail": mail,"phone":phone})
+
+        session.add(user)
+        session.commit()
+
+        print('COMMITED SESSION')
+
